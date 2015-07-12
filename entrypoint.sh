9fd7442be7f06f3b72f8988db58d2dee43f2dc21
Vijay Korapaty <github@vky.me>
2015-07-12 14:26:11 -0700

    Container works again.
    Based off of this ticket:
    https://issues.mediagoblin.org/ticket/978
    
    And the Gunicorn docs:
    http://docs.gunicorn.org/en/19.0/settings.html
    
    The `broadcast` config options are being passed via the Gunicorn
    command line option. This gets MediaGoblin to be accessible outside of
    the container.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ service postgresql start
 sudo -u postgres createuser root
 sudo -u postgres createdb -E UNICODE -O root mediagoblin --template=template0
 ./bin/gmg dbupdate
-./lazyserver.sh
+./lazyserver.sh -b 0.0.0.0:6543
